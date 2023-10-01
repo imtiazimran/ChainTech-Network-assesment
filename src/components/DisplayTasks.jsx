0cdcf9501c97f2e517a28032afe4b7e41e65af05
imtiazimran <imtiazimran@outlook.com>
2023-10-02 01:08:15 +0600

    fixed update for task description after long time

--- src/components/DisplayTasks.jsx
+++ src/components/DisplayTasks.jsx
@@ -1,18 +1,57 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import TaskCard from './shared/TaskCard';
 import { Api } from './Context/ApiContext';
 import { Link } from 'react-router-dom';
 import { ArchiveBoxArrowDownIcon } from '@heroicons/react/24/outline';
+import { useForm } from 'react-hook-form';
 
 const DisplayTasks = () => {
+    const { handleUpdate,
+        tasks,
+        isLoading,
+        handleDelete,
+        selectedTaskForUpdate,
+        setSelectedTaskForUpdate,
+        updatedData,
+        setUpdatedData } = useContext(Api);
 
-    const { tasks, isLoading } = useContext(Api)
-
+    const [isOpen, setIsOpen] = useState(false);
 
     const pendingTasks = tasks.filter(task => task.status === 'pending');
     const runningTasks = tasks.filter(task => task.status === 'running');
     const doneTasks = tasks.filter(task => task.status === 'done');
     const archiveTasks = tasks.filter(task => task.status === 'archive');
+
+    if (isOpen) {
+        document.getElementById('my_modal_4').showModal();
+    }
+    
+
+    const onCancel = () => {
+        setSelectedTaskForUpdate(null);
+        reset();
+        setIsOpen(false);
+        document.getElementById('my_modal_4').close();
+    };
+
+    const handleModal = (task) => {
+        setIsOpen(true);
+        setSelectedTaskForUpdate(task);
+        setUpdatedData(task?.description); // Set initial value for the updatedData state.
+    }
+
+    const { register, handleSubmit, reset } = useForm();
+    console.log(selectedTaskForUpdate);
+    const updateStatus = task => {
+        handleUpdate(task);
+    }
+
+    const onSubmit = (data) => {
+        if (selectedTaskForUpdate) {
+            handleUpdate({ ...selectedTaskForUpdate, description: data.description });
+        }
+        onCancel();
+    }
     return (
         <div className='container mx-auto'>
             <ul className="steps w-full">
@@ -25,19 +64,19 @@ const DisplayTasks = () => {
                 <div className="flex flex-col justify-center items-center gap-5">
                     {
                         isLoading ? <span className="loading loading-dots loading-lg"></span> :
-                            pendingTasks.map((task) => <TaskCard task={task} />)
+                            pendingTasks.map((task) => <TaskCard task={task} handleModal={handleModal} updateStatus={updateStatus} />)
                     }
                 </div>
                 <div className="flex flex-col justify-center items-center gap-5">
                     {
                         isLoading ? <span className="loading loading-dots loading-lg"></span> :
-                            runningTasks.map((task) => <TaskCard task={task} />)
+                            runningTasks.map((task) => <TaskCard task={task} handleModal={handleModal} updateStatus={updateStatus} />)
                     }
                 </div>
                 <div className="flex flex-col justify-center items-center gap-5">
                     {
                         isLoading ? <span className="loading loading-dots loading-lg"></span> :
-                            doneTasks.map((task) => <TaskCard task={task} />)
+                            doneTasks.map((task) => <TaskCard task={task} handleModal={handleModal} updateStatus={updateStatus} />)
                     }
                 </div>
 
@@ -49,6 +88,39 @@ const DisplayTasks = () => {
                     </Link>
                 </div>
             </div>
+
+            <dialog id="my_modal_4" className="modal">
+                <div className="modal-box">
+                    <form method='dialog' onSubmit={handleSubmit(onSubmit)}>
+
+                        <div className="flex flex-col mb-5">
+                            <label htmlFor="description" className="mb-2">
+                                Description
+                            </label>
+                            <input
+                                className="w-full p-5 rounded-md"
+                                type="text"
+                                id="description"
+                                {...register('description')}
+                            />
+                        </div>
+                        <div className="flex gap-3 justify-end">
+                            <button
+                                onClick={onCancel}
+                                type="button"
+                                className="btn btn-sm"
+                            >
+                                Cancel
+                            </button>
+                            <button type="submit" className="btn btn-primary btn-sm">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                    <button onClick={onCancel} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </div>
+            </dialog>
+
         </div>
     );
 };
--- src/components/shared/TaskCard.jsx
+++ src/components/shared/TaskCard.jsx
@@ -1,53 +1,7 @@
 import { ArrowRightIcon, PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline';
-import { useContext, useState } from 'react';
-import { Api } from '../Context/ApiContext';
 
-import { useForm } from 'react-hook-form';
-
-const TaskCard = ({ task }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { handleUpdate,
-    handleDelete,
-    selectedTaskForUpdate,
-    setSelectedTaskForUpdate,
-    updatedData,
-    setUpdatedData } = useContext(Api);
-
-  const { register, handleSubmit, reset } = useForm();
-  console.log(selectedTaskForUpdate);
-  const updateStatus = task => {
-    handleUpdate(task);
-  }
-
-  const onSubmit = () => {
-    if (selectedTaskForUpdate) {
-      handleUpdate({ ...selectedTaskForUpdate, description: updatedData });
-    }
-    onCancel();
-  }
-
-  const handleChange = e => {
-    setUpdatedData(e.target.value);
-    console.log(e.target.value);
-  }
-
-  const onCancel = () => {
-    setSelectedTaskForUpdate(null);
-    reset();
-    setIsOpen(false);
-    document.getElementById('my_modal_4').close();
-  };
-
-  const handleModal = (task) => {
-    setIsOpen(true);
-    setSelectedTaskForUpdate(task);
-    setUpdatedData(task?.description); // Set initial value for the updatedData state.
-  }
-
-  if (isOpen) {
-    document.getElementById('my_modal_4').showModal();
-  }
-
+const TaskCard = ({ task, handleModal, updateStatus }) => {
+  
   let updatedStatus;
 
   if (task?.status === 'pending') {
@@ -83,40 +37,6 @@ const TaskCard = ({ task }) => {
           </button>
         </div>
       </div>
-
-      <dialog id="my_modal_4" className="modal">
-        <div className="modal-box">
-          <form method='dialog' onSubmit={handleSubmit(onSubmit)}>
-
-            <div className="flex flex-col mb-5">
-              <label htmlFor="title" className="mb-2">
-                Description
-              </label>
-              <input
-                className="w-full p-5 rounded-md"
-                type="text"
-                id="description"
-                // value={updatedData} // Use value to reflect changes.
-                onChange={handleChange}
-                {...register('description')}
-              />
-            </div>
-            <div className="flex gap-3 justify-end">
-              <button
-                onClick={onCancel}
-                type="button"
-                className="btn btn-sm"
-              >
-                Cancel
-              </button>
-              <button type="submit" className="btn btn-primary btn-sm">
-                Submit
-              </button>
-            </div>
-          </form>
-          <button onClick={onCancel} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-        </div>
-      </dialog>
     </div>
   );
 };
